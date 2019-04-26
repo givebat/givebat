79b984835ea69373b06b8157f5362501dd8269b5
Daniel James <danielcolinjames@gmail.com>
2019-04-25 22:45:50 -0700

    Update section label

--- imports/ui/common/SectionLabel.jsx
+++ imports/ui/common/SectionLabel.jsx
@@ -1,71 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SectionLabelContainer = styled.div`
-    max-width: 984px;
-
-    margin-left: auto;
-    margin-right: auto;
-
-    @media(max-width: 1366px) {
-        margin-bottom: 32px;
-    }
-
-    @media(max-width: 1030px) {
-        /* because bategories required a hacky workaround because of margins */
-        padding: ${props => props.bategories ? "0 20px" : "0"};
-    }
-
-    @media(max-width: 768px) {
-        margin-left: -20px;
-    }
-`;
-
-const SectionLabel = styled.div`
-    height: 32px;
-    width: 224px;
-    border-radius: 16px;
-    background-color: #000000;
-
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-
-    /* account for an extra 4px padding on the "Featured" label (from the hacky workaround for gradient hover border) */
-    margin-bottom: ${props => props.featured ? "-20px" : "-16px"};
-    margin-left: -112px;
-
-    @media(max-width: 1366px) {
-        margin-left: 0;
-    }
-
-    @media(max-width: 768px) {
-        border-radius: 0 16px 16px 0;
-    }
-`;
-
-const SectionLabelText = styled.h2`
+const SectionLabel = styled.p`
     font-display: auto;
     font-family: 'Muli', Arial, Helvetica, sans-serif;
-    font-weight: 800;
+    font-weight: 600;
     font-style: normal;
 
-    font-size: 16px;
-    line-height: 20px;
-    color: white;
+    font-size: 14px;
+    line-height: 18px;
+    letter-spacing: 4.29px;
+    color: #ff325a;
 
-    margin-left: 16px;
+    text-decoration: none;
+    padding-bottom: 16px;
+    margin: 0;
 `;
 
-export default (props) => {
+const SectionLabelContainer = styled.div`
+    max-width: 984px;
+    margin-left: auto;
+    margin-right: auto;
+`;
+
+export default props => {
     return (
-        <SectionLabelContainer bategories={props.bategories}>
-            <SectionLabel featured={props.featured}>
-                <SectionLabelText>
-                    {props.sectionName}
-                </SectionLabelText>
-            </SectionLabel>
+        <SectionLabelContainer>
+            <SectionLabel>{props.sectionName}</SectionLabel>
         </SectionLabelContainer>
-    )
-}
+    );
+};
