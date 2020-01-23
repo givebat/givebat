cbccad35d912cfe642b199d13195bf69a2cee589
Daniel James <danielcolinjames@gmail.com>
2020-01-23 11:44:44 -0800

    comment out head code

--- pages/_document.js
+++ pages/_document.js
@@ -35,7 +35,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          {/* Global Site Tag (gtag.js) - Google Analytics
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
@@ -51,7 +51,7 @@ class MyDocument extends Document {
             });
           `,
             }}
-          />
+          /> */}
         </Head>
         <body>
           <Main />
